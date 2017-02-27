c24d95303b5c537f19d12b57c305a0d0c55bc1cb
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-02-27 16:37:58 +0100

    Adding initial App root

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Mary had a little lamb.
+      </div>
+    );
+  }
+}
+
+export default App;
