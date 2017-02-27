e9adddc6852165d302c840c3c56ec3bea1cb3964
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-02-27 16:48:12 +0100

    Adding FieldOfPlay component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import FieldOfPlay from './FieldOfPlay.js';
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Mary had a little lamb.
+        <FieldOfPlay />
       </div>
     );
   }
---
+++ src/FieldOfPlay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import "./FieldOfPlay.css";
+
+class FieldOfPlay extends Component {
+  render() {
+    return (
+      <div className="FieldOfPlay">
+        Field of Play
+      </div>
+    );
+  }
+}
+
+export default FieldOfPlay;
