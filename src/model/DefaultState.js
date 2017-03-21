c59e2f83ae2d74723478fe9fa8d5d64796155a2c
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-03-21 17:57:33 +0100

    Adding airports to state

--- src/model/DefaultState.js
+++ src/model/DefaultState.js
@@ -4,7 +4,8 @@ const DefaultState = createDefaultState();
 
 function createDefaultState() {
   return {
-    time: createDefaultTime()
+    time: createDefaultTime(),
+    airports: createAirports()
   }
 }
 
@@ -13,6 +14,25 @@ function createDefaultTime() {
     millis: Date.now(),
     tick: Time.SLOW_TICK_MILLIS
   }
+}
+
+function createAirports() {
+  return [
+    createAirport("SFO", 5, 35),
+    createAirport("MCI", 45, 33),
+    createAirport("ATL", 88, 19),
+    createAirport("JFK", 92, 66)
+  ]
+}
+
+function createAirport(code="???", positionX=50, positionY=50) {
+  return {
+    code: code,
+    position: {
+      x: positionX,
+      y: positionY
+    },
+  };
 }
 
 export default DefaultState;
