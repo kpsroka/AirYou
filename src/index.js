5c3c3508987f566d585116bb40f84e5798887252
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-03-21 14:16:43 +0100

    Tying redux into index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import AppReducer from './model/reducers/AppReducer.js';
+import DefaultState from './model/DefaultState.js';
 import './index.css';
 
+let store = createStore(AppReducer, DefaultState);
+
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
