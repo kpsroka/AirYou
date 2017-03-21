c385d074e455ac95989984f7d2dfb4701b343982
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-03-21 17:50:06 +0100

    Adding initial Airport UI components

--- src/ui/components/FieldOfPlay.js
+++ src/ui/components/FieldOfPlay.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Airport from "./Airport.js";
 import "./FieldOfPlay.css";
 
 class FieldOfPlay extends Component {
@@ -6,6 +7,10 @@ class FieldOfPlay extends Component {
     return (
       <div className="FieldOfPlay">
         Field of Play
+        <Airport code="SFO" icon="○" position={{x: 5, y: 35}}/>
+        <Airport code="MCI" icon="○" position={{x: 45, y: 33}}/>
+        <Airport code="ATL" icon="○" position={{x: 88, y: 19}}/>
+        <Airport code="JFK" icon="○" position={{x: 92, y: 66}}/>
       </div>
     );
   }
