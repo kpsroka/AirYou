33cb2f35fea224b3b5e870f4dad871fc9d8d562a
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-05-17 00:12:55 +0200

    Adding reducer/action function to add a new flight.

--- src/model/reducers/AirplaneInFlightReducer.js
+++ src/model/reducers/AirplaneInFlightReducer.js
@@ -1,14 +1,26 @@
 const AirplaneInFlightReducer = (stateAirplanesInFlight, oldTimeMillis, newTimeMillis, action) => {
   switch (action.type) {
-    case 'TIME_TICK':
+    case 'TIME_TICK': {
       return stateAirplanesInFlight.map(
-        (airplaneInFlight) => ({
+        (airplaneInFlight) => {
+          let newDistanceRemaining =
+            airplaneInFlight.distanceRemainingM -
+            (airplaneInFlight.speedMps * (newTimeMillis - oldTimeMillis)) / 1000;
+          return {
           ...airplaneInFlight,
-          distanceRemainingM:
-              airplaneInFlight.distanceRemainingM -
-              (airplaneInFlight.speedMps * (newTimeMillis - oldTimeMillis)) / 1000,
-        })
+          distanceRemainingM: newDistanceRemaining,
+        }}
       ).filter((airplaneInFlight) => airplaneInFlight.distanceRemainingM > 0);
+    }
+    case 'NEW_FLIGHT':
+      return [
+        ...stateAirplanesInFlight,
+        {
+          flight: action.payload,
+          distanceRemainingM: action.payload.distanceKm * 1000,
+          speedMps: 150
+        }
+      ];
     default:
       return stateAirplanesInFlight;
   }
--- src/model/reducers/AppReducer.js
+++ src/model/reducers/AppReducer.js
@@ -1,9 +1,16 @@
 import TimeReducer from './TimeReducer.js';
+import AirplaneInFlightReducer from './AirplaneInFlightReducer.js';
 
 const AppReducer = (state, action={}) => {
+  let newTime = TimeReducer(state.time, action);
+  let newAirplanesInFlight =
+    AirplaneInFlightReducer(state.airplanesInFlight, state.time.millis, newTime.millis, action);
+
   return Object.assign(
     {}, state,
-    { time: TimeReducer(state.time, action) })
-}
+    { time: newTime },
+    { airplanesInFlight: newAirplanesInFlight }
+  )
+};
 
 export default AppReducer;
