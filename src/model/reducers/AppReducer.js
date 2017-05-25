22c2cce98e94ee80814f2b1dfcde1217ffc9789d
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-05-25 12:47:48 +0200

    Making TimeReducer work on full state instead of state.time.

--- src/model/reducers/AppReducer.js
+++ src/model/reducers/AppReducer.js
@@ -2,7 +2,7 @@ import TimeReducer from './TimeReducer.js';
 import AirplaneInFlightReducer from './AirplaneInFlightReducer.js';
 
 const AppReducer = (state, action={}) => {
-  let newTime = TimeReducer(state.time, action);
+  let newTime = TimeReducer(state, action).time;
   let newAirplanesInFlight =
     AirplaneInFlightReducer(state.airplanesInFlight, state.time.millis, newTime.millis, action);
 
--- src/model/reducers/TimeReducer.js
+++ src/model/reducers/TimeReducer.js
@@ -1,14 +1,14 @@
 import { Time } from '../../Constants.js';
 
-const TimeReducer = (stateTime, action) => {
+const TimeReducer = (state, action) => {
   switch (action.type) {
     case 'TIME_TICK':
-      let timeUpdate = createTimeUpdate(stateTime.millis, stateTime.millis + stateTime.tick);
-      return applyTimeUpdate(stateTime, timeUpdate);
+      let timeUpdate = createTimeUpdate(state.time.millis, state.time.millis + state.time.tick);
+      return updateTimeMillis(state, timeUpdate);
     case 'TIME_TICK_CHANGE':
-      return { ...stateTime, tick: getNextTick(stateTime.tick) };
+      return updateTimeTick(state);
     default:
-      return stateTime;
+      return state;
   }
 };
 
@@ -16,8 +16,20 @@ function createTimeUpdate(oldMillis, newMillis) {
   return { oldMillis: oldMillis, newMillis: newMillis }
 }
 
-function applyTimeUpdate(stateTime, timeUpdate) {
-  return { ...stateTime, millis: timeUpdate.newMillis };
+function updateTimeMillis(state, timeUpdate){
+  return { ...state, time: updateMillis(state.time, timeUpdate.newMillis)}
+}
+
+function updateMillis(stateTime, newMillis) {
+  return { ...stateTime, millis: newMillis };
+}
+
+function updateTimeTick(state) {
+  return { ...state, time: updateTick(state.time, getNextTick(state.time.tick)) };
+}
+
+function updateTick(stateTime, nextTick) {
+  return { ...stateTime, tick: nextTick };
 }
 
 function getNextTick(currentTick) {
