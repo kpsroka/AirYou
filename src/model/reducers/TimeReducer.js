da2653fe06b16ca50aae18d58e016df3d863d809
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-05-25 12:21:22 +0200

    Structuring time update a bit better.

--- src/model/reducers/TimeReducer.js
+++ src/model/reducers/TimeReducer.js
@@ -3,13 +3,22 @@ import { Time } from '../../Constants.js';
 const TimeReducer = (stateTime, action) => {
   switch (action.type) {
     case 'TIME_TICK':
-      return { ...stateTime, millis: stateTime.millis + stateTime.tick };
+      let timeUpdate = createTimeUpdate(stateTime.millis, stateTime.millis + stateTime.tick);
+      return applyTimeUpdate(stateTime, timeUpdate);
     case 'TIME_TICK_CHANGE':
       return { ...stateTime, tick: getNextTick(stateTime.tick) };
     default:
       return stateTime;
   }
 };
+
+function createTimeUpdate(oldMillis, newMillis) {
+  return { oldMillis: oldMillis, newMillis: newMillis }
+}
+
+function applyTimeUpdate(stateTime, timeUpdate) {
+  return { ...stateTime, millis: timeUpdate.newMillis };
+}
 
 function getNextTick(currentTick) {
   switch (currentTick) {
