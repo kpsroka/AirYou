4dd92e5ff888f352b70c4304d87525662b91c042
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-06-05 16:48:36 +0200

    More test methods for TimeReducer.

--- src/model/reducers/TimeReducer.test.js
+++ src/model/reducers/TimeReducer.test.js
@@ -1,6 +1,7 @@
 import TimeReducer from './TimeReducer.js';
 import DefaultState from '../DefaultState.js';
 import { Time } from '../../Constants.js';
+import { CreateFlightFn, CreateFlightScheduleFn } from '../State.js';
 
 it("TimeReducer updates time according to tick on TIME_TICK", () => {
   let state = DefaultState;
@@ -13,6 +14,30 @@ it("TimeReducer updates time according to tick on TIME_TICK", () => {
   newState.time.tick = 42;
   let newerState = TimeReducer(newState, {type: 'TIME_TICK'});
   expect(newerState.time.millis).toBe(newState.time.millis + newState.time.tick);
+});
+
+it("TimeReducer updates airplanes in flight on TIME_TICK", () => {
+  let state = DefaultState;
+  state.airplanesInFlight = [
+    {
+      flight: CreateFlightFn("AB", "123", "SFO", "LAX", CreateFlightScheduleFn("AB123", 0, 0)),
+      distanceRemainingM: 100,
+      speedMps: 250
+    }
+  ];
+  state.time.tick = 100;  // 0.1 second
+
+  let newState = TimeReducer(state, {type: 'TIME_TICK'});
+  expect(newState.airplanesInFlight).toHaveLength(1);
+  expect(newState.airplanesInFlight[0]).toEqual({
+    flight: state.airplanesInFlight[0].flight,
+    distanceRemainingM: 75,
+    speedMps: 250
+  });
+
+  newState.time.tick = 1000;  // 1 second
+  let newerState = TimeReducer(newState, {type: 'TIME_TICK'});
+  expect(newerState.airplanesInFlight).toHaveLength(0);
 });
 
 it("TimeReducer changes slow tick to fast on TIME_TICK_CHANGE", () => {
