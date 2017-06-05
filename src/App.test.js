ea775138cc2f7ec48f54780af36e180abf6e4599
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-06-05 16:13:23 +0200

    Restoring jest testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
+import AppReducer from './model/reducers/AppReducer.js';
+import DefaultState from './model/DefaultState.js';
 
 it('renders without crashing', () => {
+  let store = createStore(AppReducer, DefaultState);
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
 });
