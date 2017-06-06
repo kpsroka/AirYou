99992e0f54063386a51c9fa8557d0d5370d6857f
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-06-06 12:55:54 +0200

    Adding a test for ScheduleReducer

---
+++ src/model/reducers/ScheduleReducer.test.js
@@ -0,0 +1,20 @@
+import ScheduleReducer from './ScheduleReducer.js';
+import { CreateFlightFn, CreateFlightScheduleFn } from '../State.js';
+
+it("ScheduleReducer removes schedule with matching flight code", () => {
+  let flights = [
+      CreateFlightFn("XX", "101", "JFK", "ORD", CreateFlightScheduleFn("XX101", 1, 1)),
+      CreateFlightFn("XX", "102", "ORD", "JFK", CreateFlightScheduleFn("XX102", 12, 12))
+  ];
+
+  let newFlights = ScheduleReducer(flights, {type:'DELETE_SCHEDULE', payload:'XX101'});
+  expect(newFlights).toHaveLength(1);
+  expect(newFlights[0]).toEqual(flights[1]);
+
+  let newerFlights =  ScheduleReducer(newFlights, {type:'DELETE_SCHEDULE', payload:'YY999'});
+  expect(newerFlights).toEqual(newFlights);
+
+  let newestFlights = ScheduleReducer(newFlights, {type:'DELETE_SCHEDULE', payload:'XX102'});
+  expect(newestFlights).toHaveLength(0);
+});
+
