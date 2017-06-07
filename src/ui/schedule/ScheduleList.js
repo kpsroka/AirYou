2142935159da1081087037d89d6619bdc448de7a
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-06-07 13:19:29 +0200

    Styling improvements in ScheduleList.

--- src/ui/schedule/ScheduleList.js
+++ src/ui/schedule/ScheduleList.js
@@ -4,6 +4,35 @@ import "./ScheduleList.css";
 let DAYS_OF_WEEK_RANGE = [0, 1, 2, 3, 4, 5, 6];
 
 class ScheduleList extends Component {
+  render() {
+    return (
+      <div className="scheduleList">
+        <div className="scheduleListClose" onClick={() => this.props.onCloseWindowRequest()}>✖</div>
+        {this.props.flights.map((flight) => {
+          return (
+            <div
+              key={flight.flightCode}
+              className="scheduleListItem">
+              <div className="scheduleListItemFlightCode">{flight.flightCode}</div>
+              <div className="scheduleListItemRoute">
+                {flight.route.departureAirportCode}-{flight.route.arrivalAirportCode}
+              </div>
+              <div className="scheduleListItemDepartureTime">
+                {this.formatTime(flight.schedule.departureHours, flight.schedule.departureMinutes)}
+              </div>
+              <div>
+                {DAYS_OF_WEEK_RANGE.map((dayOfWeek) => (
+                  this.getDayOfWeekTag(dayOfWeek, flight.schedule)
+                ))}
+              </div>
+              <div onClick={() => this.deleteSchedule(flight.flightCode)}>Delete</div>
+            </div>
+          )
+        })}
+      </div>
+    );
+  }
+
   getDayOfWeekTag(dayOfWeek, schedule) {
     let tagClassName = (schedule.departureDaysOfWeek.indexOf(dayOfWeek) === -1)
         ? "inactiveDayOfWeek" : "activeDayOfWeek";
@@ -16,31 +45,6 @@ class ScheduleList extends Component {
     let timeFormatOptions = { weekday: "narrow" };
     /* May 1 2017 was Monday, so the day of May 2017 will match the desired day of week. */
     return Intl.DateTimeFormat("en-US", timeFormatOptions).format(new Date(2017, 4, dayOfWeek));
-  }
-
-  render() {
-    return (
-      <div className="scheduleList">
-        <div className="scheduleListClose" onClick={() => this.props.onCloseWindowRequest()}>✖</div>
-        {this.props.flights.map((flight) => {
-          return (
-            <div
-              key={flight.flightCode}
-              className="scheduleListItem">
-              <div>{flight.flightCode}</div>
-              <div>{flight.route.departureAirportCode}-{flight.route.arrivalAirportCode}</div>
-              <div>{this.formatTime(flight.schedule.departureHours, flight.schedule.departureMinutes)}</div>
-              <div>
-                {DAYS_OF_WEEK_RANGE.map((dayOfWeek) => (
-                  this.getDayOfWeekTag(dayOfWeek, flight.schedule)
-                ))}
-              </div>
-              <div onClick={() => this.deleteSchedule(flight.flightCode)}>Delete</div>
-            </div>
-          )
-        })}
-      </div>
-    );
   }
 
   deleteSchedule(flightId) {
