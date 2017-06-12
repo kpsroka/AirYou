d9f31de36559ff4e130d21a1c4e493178d0a194c
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-06-12 14:15:38 +0200

    Adding Save/Cancel buttons.

--- src/ui/schedule/ScheduleEditorRow.js
+++ src/ui/schedule/ScheduleEditorRow.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './ScheduleEditorRow.css';
 
 class ScheduleEditorRow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      editMode: false,
+      validInput: true,
+    }
+  }
+
   render() {
     return (
       <div className="scheduleEditorRow">
@@ -9,9 +17,37 @@ class ScheduleEditorRow extends React.Component {
         <div className="scheduleEditorValue">
           {this.renderValue()}
         </div>
-        <div className="scheduleEditorEditButton">✍</div>
+        <div className={this.getEditButtonClassNames()}
+             onClick={() => this.setEditMode(true)}>
+          <div>✍</div>
+          <div>EDIT</div>
+        </div>
+        <div className={this.getSaveButtonClassNames()}>
+          <div>✓</div>
+          <div>SAVE</div>
+        </div>
+        <div className={this.getCancelButtonClassNames()}>
+          <div>✖</div>
+          <div>ABORT</div>
+        </div>
       </div>
     );
+  }
+
+  getEditButtonClassNames() {
+    return "scheduleEditorButton" + (this.state.editMode ? " hidden" : "");
+  }
+
+  getSaveButtonClassNames() {
+    return "scheduleEditorButton" + (this.state.editMode ? "" : " hidden");
+  }
+
+  getCancelButtonClassNames() {
+    return "scheduleEditorButton" + (this.state.editMode ? "" : " hidden");
+  }
+
+  setEditMode(value) {
+    this.setState({editMode: value});
   }
 
   renderValue() {
