b072cf58fd69665d062003f188739beba1f3a9a2
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-06-16 11:31:01 +0200

    Updating ScheduleReducer tests.

--- src/model/reducers/ScheduleReducer.test.js
+++ src/model/reducers/ScheduleReducer.test.js
@@ -1,7 +1,7 @@
 import ScheduleReducer from './ScheduleReducer.js';
 import { CreateFlightFn, CreateFlightScheduleFn, CreateRouteFn } from '../State.js';
 
-it("ScheduleReducer removes schedule with matching flight code", () => {
+it("ScheduleReducer removes schedule with given index", () => {
   let flights = [
       CreateFlightFn("101", CreateRouteFn("JFK", "ORD"), CreateFlightScheduleFn(1, 1)),
       CreateFlightFn("102", CreateRouteFn("ORD", "JFK"), CreateFlightScheduleFn(12, 12))
@@ -17,4 +17,20 @@ it("ScheduleReducer removes schedule with matching flight code", () => {
   let newestFlights = ScheduleReducer(newFlights, {type:'DELETE_SCHEDULE', payload:1});
   expect(newestFlights).toHaveLength(2);
   expect(newestFlights).toEqual([,,]);
+});
+
+it("ScheduleReducer saves provided schedule", () => {
+  let flights = [
+    CreateFlightFn("101", CreateRouteFn("JFK", "ORD"), CreateFlightScheduleFn(1, 1)),
+    CreateFlightFn("102", CreateRouteFn("ORD", "JFK"), CreateFlightScheduleFn(12, 12)),
+    CreateFlightFn("103", CreateRouteFn("MIA", "BOS"), CreateFlightScheduleFn(2, 2))
+  ];
+
+  let newFlight = CreateFlightFn("399", CreateRouteFn("BOS", "MIA"), CreateFlightScheduleFn(15, 15));
+
+  let updatedFlights = ScheduleReducer(
+      flights,
+      {type:'SAVE_SCHEDULE', payload:{index:1,flight:newFlight}});
+
+  expect(updatedFlights).toEqual([flights[0], newFlight, flights[2]]);
 });
