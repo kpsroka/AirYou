8cac6513f5ea2ddf2dbc6a4a91c37fcf5a7ccef2
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-06-26 12:51:06 +0200

    Extending test suite for ScheduleEditor.

--- src/ui/schedule/ScheduleEditor.test.js
+++ src/ui/schedule/ScheduleEditor.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import sinon from 'sinon';
 import { shallow } from 'enzyme';
 import ScheduleEditor from './ScheduleEditor.js';
 
@@ -8,5 +9,19 @@ describe("ScheduleEditor", () => {
     const scheduleEditor = shallow(<ScheduleEditor title={testString} />);
     expect(scheduleEditor.find(".modalWindowTitle").length).toEqual(1);
     expect(scheduleEditor.find(".modalWindowTitle").text()).toEqual(testString);
+  });
+
+  test("launches provided fn on close click", () => {
+    const onClickSpy = sinon.spy();
+    const scheduleEditor = shallow(<ScheduleEditor onCloseWindowRequest={onClickSpy} />);
+    expect(scheduleEditor.find(".modalWindowClose").length).toEqual(1);
+
+    const scheduleCloseLabel = scheduleEditor.find(".modalWindowClose").at(0);
+
+    scheduleCloseLabel.simulate("click");
+    expect(onClickSpy.callCount).toEqual(1);
+
+    scheduleCloseLabel.simulate("click");
+    expect(onClickSpy.callCount).toEqual(2);
   });
 });
