16feeb17b38b749ed47fc2ae3e888f470e1fdd5a
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-06-26 13:09:00 +0200

    Extending test suite for ScheduleEditor.

--- src/ui/schedule/ScheduleEditor.test.js
+++ src/ui/schedule/ScheduleEditor.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import sinon from 'sinon';
 import { shallow } from 'enzyme';
 import ScheduleEditor from './ScheduleEditor.js';
+import ScheduleEditorRowComponent from './ScheduleEditorRowComponent.js';
 
 describe("ScheduleEditor", () => {
   test("displays title from props", () => {
@@ -23,5 +24,17 @@ describe("ScheduleEditor", () => {
 
     scheduleCloseLabel.simulate("click");
     expect(onClickSpy.callCount).toEqual(2);
+  });
+
+  test("passes flightIndex to child rows", () => {
+    const testIndex = 321;
+    const scheduleEditor = shallow(<ScheduleEditor flightIndex={testIndex} />);
+    const childRows = scheduleEditor.find(ScheduleEditorRowComponent);
+
+    expect(childRows.length).toBeGreaterThan(0);  // If this fails, the test is useless.
+
+    for (let i = 0; i < childRows.length; i++) {
+      expect(childRows.at(i).props().flightIndex).toEqual(testIndex);
+    }
   });
 });
