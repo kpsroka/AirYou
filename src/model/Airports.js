c809a445fc0f99837a81f735a89fec07736fc521
Krzysztof Sroka <kpsroka@users.noreply.github.com>
2017-07-13 13:00:01 +0200

    Ordering airports by iata code, adding size param.

--- src/model/Airports.js
+++ src/model/Airports.js
@@ -4,42 +4,45 @@ import type { Position } from './Position.js';
 
 export type Airport = {
   code:string,
-  position:Position
+  position:Position,
+  size:number
 };
 
 export const AIRPORTS = [
-  createAirport("SFO", 3, 59),
-  createAirport("LAX", 8, 42),
-  createAirport("PDX", 7, 89),
-  createAirport("SEA", 10, 95),
-  createAirport("SLC", 22, 62),
-  createAirport("PHX", 19, 37),
-  createAirport("DEN", 33, 67),
-  createAirport("MCI", 52, 53),
-  createAirport("DFW", 48, 30),
-  createAirport("MSP", 54, 74),
-  createAirport("ORD", 63, 65),
-  createAirport("DTW", 71, 67),
-  createAirport("MEM", 63, 44),
-  createAirport("ATL", 72, 35),
-  createAirport("MIA", 82, 6),
-  createAirport("CLT", 75, 42),
-  createAirport("PIT", 77, 61),
-  createAirport("IAD", 82, 58),
-  createAirport("BOS", 91, 73),
-  createAirport("JFK", 88, 66)
+  createAirport("ATL", 72, 35, 101),
+  createAirport("BOS", 91, 73, 33),
+  createAirport("CLT", 75, 42, 44),
+  createAirport("DEN", 33, 67, 54),
+  createAirport("DFW", 48, 30, 64),
+  createAirport("DTW", 71, 67, 33),
+  createAirport("IAD", 82, 58, 21),
+  createAirport("JFK", 88, 66, 56),
+  createAirport("LAX", 8, 42, 74),
+  createAirport("MCI", 52, 53, 10),
+  createAirport("MEM", 63, 44, 3),
+  createAirport("MIA", 82, 6, 44),
+  createAirport("MSP", 54, 74, 36),
+  createAirport("ORD", 63, 65, 76),
+  createAirport("PDX", 7, 89, 16),
+  createAirport("PHX", 19, 37, 44),
+  createAirport("PIT", 77, 61, 8),
+  createAirport("SEA", 10, 95, 42),
+  createAirport("SFO", 3, 59, 50),
+  createAirport("SLC", 22, 62, 22)
 ];
 
 export function createAirport(
     code:string,
     positionX:number,
-    positionY:number)
+    positionY:number,
+    size:number)
     :Airport {
   return {
     code: code,
     position: {
       x: positionX,
       y: positionY
-    }
+    },
+    size: size,
   }
 }
